eff8ff1a8489c7f6b2d77fbdc1a08849b8cc2fdc
TheGloved1 <gloves1229@gmail.com>
2024-02-21 21:13:18 -0600

    Revert "create 1 file, update 5 files and delete 2 files"
    This reverts commit 7e9c1e2ec9065a4f85186d222c899bb482333571.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 
 import { createApp } from "vue";
 import VueObserveVisibility from "vue-observe-visibility";
-// const { GoogleGenerativeAI } = require("https://esm.run/@google/generative-ai");
 import App from "./App.vue";
 
 const app = createApp(App);
